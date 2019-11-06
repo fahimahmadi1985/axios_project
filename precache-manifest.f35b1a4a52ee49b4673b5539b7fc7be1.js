a3d1243a2edc744bed1001a14f047b2687a3c661
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-06 14:13:55 +0100

    Updates

---
+++ precache-manifest.f35b1a4a52ee49b4673b5539b7fc7be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f33e9cc58343c781db38a8adf4aa4e",
+    "url": "/axios_project/index.html"
+  },
+  {
+    "revision": "c3991f753f2ac3b0c3df",
+    "url": "/axios_project/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b4ca36c8e82e8ad43370",
+    "url": "/axios_project/static/js/2.b80a647e.chunk.js"
+  },
+  {
+    "revision": "c3991f753f2ac3b0c3df",
+    "url": "/axios_project/static/js/main.ed470ef3.chunk.js"
+  },
+  {
+    "revision": "a5deece5135199629d27",
+    "url": "/axios_project/static/js/runtime-main.c5bea974.js"
+  }
+]);
