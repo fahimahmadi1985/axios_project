7c4ea118093b95fbb9603a9664173df82be8e029
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-07 12:15:57 +0100

    after changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [term, setTerm] = useState("");
   const [pictures, setPictures] = useState([]);
+  const [pageNum, setPagenum] = useState(1);
+  const [shouldRef, setShouldRef] = useState(false);
 
   const changeHandler = e => {
     setTerm(e.target.value);
     console.log(term);
   };
+
+  const pageChange = e => {
+    e.preventDefault();
+    e.target.name === "inc"
+      ? setPagenum(pageNum + 1)
+      : e.target.name === "dec" &&
+        pageNum > 1 &&
+        /* hey omar the next line is a ternary condition */ setPagenum(
+          pageNum - 1
+        );
+    setShouldRef(true);
+    console.log("pageNum: ", pageNum);
+  };
+
+  useEffect(() => {
+    shouldRef &&
+      axios
+        .get("https://api.unsplash.com/search/photos", {
+          params: {
+            query: term,
+            page: pageNum,
+            per_page: 21
+          },
+          headers: {
+            Authorization:
+              "Client-ID 89a74f8c26da940b295f7c22ccaf83e3404ac033065c8db15fcbbc3b0639a400"
+          }
+        })
+        .then(response => {
+          setPictures([...response.data.results]);
+          setShouldRef(false);
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+  });
 
   const sendRequest = e => {
     e.preventDefault();
@@ -16,8 +54,7 @@ function App() {
       .get("https://api.unsplash.com/search/photos", {
         params: {
           query: term,
-          page: 2,
-          per_page: 15
+          per_page: 21
         },
         headers: {
           Authorization:
@@ -54,31 +91,36 @@ function App() {
         </form>
       </div>
       <div className="container-fluid">
-        <table className="table mt-5 border-0">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Picture</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {pictures.length
-              ? pictures.map(pic => (
-                  <tr key={pic.id}>
-                    <td>{pic.id}</td>
-                    <td>
-                      <img
-                        src={pic.urls.thumb}
-                        alt={pic.id}
-                        className="rounded"
-                      />
-                    </td>
-                  </tr>
-                ))
-              : null}
-          </tbody>
-        </table>
+        <div className="d-flex flex-wrap">
+          {pictures.length
+            ? pictures.map(pic => (
+                <img
+                  src={pic.urls.thumb}
+                  alt={pic.alt_description}
+                  key={pic.id}
+                  style={{ width: "100px", height: "120px" }}
+                />
+              ))
+            : null}
+        </div>
+        <div className="d-flex justify-content-between">
+          <button
+            type="button"
+            onClick={pageChange}
+            name="dec"
+            className="form-control col-3 btn btn-primary"
+          >
+            {"<<"}
+          </button>
+          <button
+            type="button"
+            onClick={pageChange}
+            name="inc"
+            className="form-control col-3 btn btn-primary"
+          >
+            {">>"}
+          </button>
+        </div>
       </div>
     </div>
   );
